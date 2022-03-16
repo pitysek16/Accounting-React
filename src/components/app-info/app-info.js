8f43df2c0db1b8cc71f0fb7a9eacdaf6bc5aa084
Katerina Pidan <pitysek@bk.ru>
2022-03-16 11:36:39 +0200

    update: dynamic info in header

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import "./app-info.css";
 
-const AppInfo = () => {
+const AppInfo = ({ increased, employees }) => {
   return (
     <div className="app-info">
       <h1>Accounting for employees in company</h1>
-      <p>Total number of employees:</p>
-      <p>The promotion will be given to:</p>
+      <p>Total number of employees: {employees}</p>
+      <p>The promotion will be given to: {increased}</p>
     </div>
   );
 };
