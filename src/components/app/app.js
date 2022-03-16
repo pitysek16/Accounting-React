608a6dcaa6f39544b63b4fea73b68cf2f1e6c2df
Katerina Pidan <pitysek@bk.ru>
2022-03-16 10:51:59 +0200

    update: add new employee, delete employee

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,27 +8,58 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 import "./app.css";
 
-function App() {
-  const data = [
-    { name: "John Snow", salary: 800, increase: false, id: 1 },
-    { name: "Robb Stark", salary: 3000, increase: true, id: 2 },
-    { name: "Arya Stark", salary: 5000, increase: false, id: 3 },
-    { name: "Daenerys Targaryen", salary: 900, increase: false, id: 4 },
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "John Snow", salary: 800, increase: false, id: 1 },
+        { name: "Robb Stark", salary: 3000, increase: true, id: 2 },
+        { name: "Arya Stark", salary: 5000, increase: false, id: 3 },
+      ],
+    };
+    this.maxId = 4;
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm onAdd={this.addItem} />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
